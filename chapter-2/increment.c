fa4cb0225e702e0268656a8ed03fa09701ead9fb
GameDog9988 <alex@grattan.me>
2023-10-09 14:57:42 -0400

    feat: add retrieve function to increment

--- chapter-2/increment.c
+++ chapter-2/increment.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
+static unsigned int counter = 0;
+
 void increment(void) {
-  static unsigned int counter = 0;
   counter++;
   printf("%d ", counter);
+}
+
+void retrieve (void) {
+  printf("Retrieve: %d", counter);
 }
 
 int main(void) {
   for (int i = 0; i < 5; i++) {
     increment();
   }
+  retrieve();
   return 0;
 }
